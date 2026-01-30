831bb8547616dc2f781e1d52004b36ca6caaa135
Vadym <vadym.rebrov@gmail.com>
2026-01-30 11:21:05 +0200

    Add consul

--- src/main/java/dev/profitsoft/internship/rebrov/blocktwo/config/SecurityConfig.java
+++ src/main/java/dev/profitsoft/internship/rebrov/blocktwo/config/SecurityConfig.java
@@ -16,6 +16,7 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/actuator/**", "/health").permitAll()
                         .anyRequest().authenticated()
                 )
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
