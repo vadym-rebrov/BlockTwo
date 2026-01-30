fbeef006a18cf4b01e43d09ac0aba067beb12d4f
Vadym <vadym.rebrov@gmail.com>
2026-01-30 11:56:21 +0200

    fix config

--- src/main/java/dev/profitsoft/internship/rebrov/blocktwo/config/SecurityConfig.java
+++ src/main/java/dev/profitsoft/internship/rebrov/blocktwo/config/SecurityConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -15,6 +17,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
+                .cors(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/actuator/**", "/health").permitAll()
                         .anyRequest().authenticated()
