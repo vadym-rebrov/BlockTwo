8ffa60577517e7eb375b1615f0053ead790c7e04
Vadym <vadym.rebrov@gmail.com>
2026-02-01 12:53:07 +0200

    Add cors config

--- src/main/java/dev/profitsoft/internship/rebrov/blocktwo/config/SecurityConfig.java
+++ src/main/java/dev/profitsoft/internship/rebrov/blocktwo/config/SecurityConfig.java
@@ -33,7 +33,7 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:9000", "http://localhost:3000"));
+        configuration.setAllowedOrigins(List.of("http://localhost:9000", "http://localhost:3000", "http://localhost:3500"));
         configuration.setAllowedMethods(List.of("*"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
